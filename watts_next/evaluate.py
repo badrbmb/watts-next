9c7ccef6bc31c576a08329f16d1d7829d6360111
Badr Ben m'barek <badr.benb@gmail.com>
2024-01-26 22:49:39 +0000

    feat: add slicing functions for eval

--- watts_next/evaluate.py
+++ watts_next/evaluate.py
@@ -1,13 +1,17 @@
+from typing import Callable
+
 import numpy as np
+import pandas as pd
 import sklearn.metrics
 import structlog
+from snorkel.slicing import PandasSFApplier, SlicingFunction, slicing_function
 
 logger = structlog.get_logger()
 
 
 def get_overall_metrics(
-    y_true: np.ndarray,
-    y_pred: np.ndarray,
+    y_true: pd.Series,
+    y_pred: pd.Series,
 ) -> dict[str, np.float64]:
     """Get overall performance metrics."""
     mask = ~np.isnan(y_pred)
@@ -39,3 +43,82 @@ def get_overall_metrics(
     )
 
     return overall_metrics
+
+
+class SlicingFunctionsRepository:
+    def __init__(
+        self,
+        min_cut_off: float = 0.1,
+        max_cut_off: float = 0.9,
+    ) -> None:
+        self.min_cut_off = min_cut_off
+        self.max_cut_off = max_cut_off
+
+    @staticmethod
+    def is_cold(x: pd.Series, cut_off: float) -> bool:
+        """Slicing function for cold timestamps."""
+        return x["t2m"] <= cut_off
+
+    @staticmethod
+    def is_hot(x: pd.Series, cut_off: float) -> bool:
+        """Slicing function for hot timestamps."""
+        return x["t2m"] >= cut_off
+
+    @staticmethod
+    def is_windy(x: pd.Series, cut_off: float) -> bool:
+        """Slicing function for windy timestamps."""
+        return abs(x["u10"]) >= cut_off or abs(x["v10"]) >= cut_off
+
+    @staticmethod
+    def is_wet(x: pd.Series, cut_off: float) -> bool:
+        """Slicing function for wet timestamps."""
+        return x["tp"] >= cut_off
+
+    @slicing_function()
+    @staticmethod
+    def is_holidays(x: pd.Series) -> bool:
+        """Slicing function for holiday timestamps."""
+        return x["holiday"] != "NA"
+
+    @staticmethod
+    def make_cut_off_slicing_function(function: Callable, cutoff: float) -> SlicingFunction:
+        """Create the slicing functions with their mathcing cut-offs."""
+        return SlicingFunction(
+            name=function.__name__,
+            f=function,
+            resources={"cut_off": cutoff},
+        )
+
+    def get_all_slicing_function(self, df_data: pd.DataFrame) -> list[SlicingFunction]:
+        """Dynamically define cut-off based slicing functions."""
+        cut_offs = {}
+        cut_offs["is_cold"] = df_data["t2m"].quantile(self.min_cut_off)
+        cut_offs["is_hot"] = df_data["t2m"].quantile(self.max_cut_off)
+        cut_offs["is_wet"] = df_data["tp"].quantile(self.max_cut_off)
+        cut_offs["is_windy"] = max(
+            [
+                df_data["u10"].quantile(self.max_cut_off),
+                df_data["v10"].quantile(self.max_cut_off),
+            ],
+        )
+
+        all_slicing_functions = [self.is_holidays]
+        for function in [
+            self.is_cold,
+            self.is_hot,
+            self.is_wet,
+            self.is_windy,
+        ]:
+            all_slicing_functions.append(
+                self.make_cut_off_slicing_function(
+                    function=function,
+                    cutoff=cut_offs[function.__name__],
+                ),
+            )
+        return all_slicing_functions
+
+    @staticmethod
+    def get_slices(all_slicing_functions: list[SlicingFunction], df: pd.DataFrame) -> np.ndarray:
+        """Comptue slices given a list of slicing functions and df."""
+        # TODO: issue with snorkel typing SlicingFunction <> LabellingFunction
+        return PandasSFApplier(all_slicing_functions).apply(df)  # type: ignore
