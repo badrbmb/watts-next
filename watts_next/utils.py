9e3640559f0f61a5a6057fb68dab95a4e56c9a3b
Badr Ben m'barek <badr.benb@gmail.com>
2024-01-26 13:02:50 +0000

    chore: add utils for wear ref lru caching

--- watts_next/utils.py
+++ watts_next/utils.py
@@ -1,10 +1,33 @@
+import functools
+import weakref
 from pathlib import Path
+from typing import Any, Callable
 
 import geopandas as gpd
 import pandas as pd
 import requests
-import shapely
-from shapely.geometry.base import BaseGeometry
+
+
+def weak_lru(
+    maxsize: int = 128,
+    typed: bool = False,
+) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+    """LRU Cache decorator that keeps a weak reference to "self"."""
+
+    def wrapper(func: Callable[..., Any]) -> Callable[..., Any]:
+        @functools.lru_cache(maxsize, typed)
+        def _func(_self: weakref.ref, *args: Any, **kwargs: Any) -> Any:  # noqa: ANN401
+            # Dereference the weakref and call the original function
+            return func(_self(), *args, **kwargs)
+
+        @functools.wraps(func)
+        def inner(self: Any, *args: Any, **kwargs: Any) -> Any:  # noqa: ANN401
+            # Create a weakref to 'self' and call the cached function
+            return _func(weakref.ref(self), *args, **kwargs)
+
+        return inner
+
+    return wrapper
 
 
 def download_file_from_url(url: str, output_path: Path) -> None:
@@ -17,11 +40,10 @@ def download_file_from_url(url: str, output_path: Path) -> None:
             file.write(chunk)
 
 
-def load_geometry_from_geojsons(paths: list[Path]) -> BaseGeometry:
+def load_geometry_from_geojsons(paths: list[Path]) -> gpd.GeoDataFrame:
     """Load all geosjon files and return unary union."""
     geo_df = pd.concat(
         [gpd.read_file(path) for path in paths],
     )
     geo_df = geo_df.to_crs("EPSG:4326")  # type: ignore
-
-    return shapely.unary_union(geo_df["geometry"].tolist())
+    return geo_df
